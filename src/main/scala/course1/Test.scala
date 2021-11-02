0886c7a668277731ecd59735e875c79ce086738f
jenniferliu <jennifer.c.oakville@gmail.com>
2021-11-02 13:09:41 -0400

    Jennifer Udpate

--- src/main/scala/course1/Test.scala
+++ src/main/scala/course1/Test.scala
@@ -15,13 +15,12 @@ object Test extends App {
   }.toArray
 
 
-  //def lambda(in: SaleDetails): Boolean = in.amount > 5000
+  def lambda(in: SaleDetails): Boolean = in.amount > 5000
   val transferred = listOfSaleDetails.map(_.amount).filter(_ > 5000)
   println(transferred)
-
-//  println(s"Number of deals over 5000 is ${listOfSaleDetails.count(_.amount > 5000)}")
-//  println(s"Max amount deal is ${listOfSaleDetails.map(_.amount).max}")
-//  println(listOfSaleDetails.size)
+  println(s"Number of deals over 5000 is ${listOfSaleDetails.count(_.amount > 5000)}")
+  println(s"Max amount deal is ${listOfSaleDetails.map(_.amount).max}")
+  println(listOfSaleDetails.size)
   /*
   //printf("2004-01-09T17:28:15.618-05:00".dateNumber.toString)
   val maxSaleByDay = listOfSaleDetails.groupBy(_.time.dateNumber).map{
