9d897217e7a0ae8f27b3ae401a58a3f12e931bfe
oscargee <zhiwilliam@gmail.com>
2021-11-10 17:49:05 -0500

    added generic tool test

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -11,16 +11,36 @@ object Test extends App {
   val tree = Node("1", Node("2", Node("4"), Node("5")), Node("3", Node("0", Node("9"), null), Node("6", Node("7"), null)))
   import binarytreetools.Actions._
 
-  println(tree.dsfTreeTraverse[Vector, String](DsfOrders.Inorder,
+  // traverse
+  println(tree.dsfTraverse[Vector, String](DsfOrders.Inorder,
     (node, result) => result :+ node.value
   ).mkString(","))
 
-  println(tree.dsfTreeTraverse[Option, String](DsfOrders.Postorder,
+  // find max value in a tree
+  println(tree.dsfTraverse[Option, String](DsfOrders.Postorder,
     (node, result) => result match {
       case None => Option(node.value)
       case Some(max) => if (node.value > max) Option(node.value) else Option(max)
     }
   ))
+  implicit def compareCalc(f: => Boolean) = new {
+    def ?[A] (a: => A, b: => A): A = {
+      if(f) a else b
+    }
+  }
+  // find layers
+  println(tree.dsfPostOrderSearch[Option, Int](
+    (node, lr, rr) => (lr, rr) match {
+      case (None, None) => Some(1)
+      case (Some(layer), None) => Some(layer + 1)
+      case (None, Some(layer)) => Some(layer + 1)
+      case (Some(ly), Some(ry)) => (ly < ry) ? (Some(ry + 1), Some(ly + 1))
+    }
+  ))
+
+
+
+
 
   val value: Unit = tree.prettyPrint()
 }
