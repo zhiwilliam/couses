9f6951f7fb1f744165c1d00198f23b13c4ca0976
oscargee <zhiwilliam@gmail.com>
2022-01-16 20:16:08 -0500

    Added map tool

--- src/main/scala/course3/MapTools.scala
+++ src/main/scala/course3/MapTools.scala
@@ -1,30 +1,33 @@
 package course3
 
+trait MapTools[F[_], K, V] {
+  def filterByValues(condition: V => Boolean)
+                    (implicit f: F[V] => TraversableGeneric[F, V]): Map[K, F[V]]
+  def partitionByValues(condition: V => Boolean)
+                       (implicit f: F[V] => TraversableGeneric[F, V]): (Map[K, F[V]], Map[K, F[V]])
+}
 object MapTools {
   def filterMapByValues[F[_], K, V](map: Map[K, F[V]], condition: V => Boolean)
-                                (implicit iter: F[V] => TraversableGeneric[F, V]): Map[K, F[V]] = {
+                                (implicit f: F[V] => TraversableGeneric[F, V]): Map[K, F[V]] = {
     map.map { case (key, values) =>
-      key -> iter(values).filter(condition)
+      key -> f(values).filter(condition)
     }.filter(_._2.nonEmpty)
   }
   def partitionMapByValues[F[_], K, V](map: Map[K, F[V]], condition: V => Boolean)
-                                   (implicit iter: F[V] => TraversableGeneric[F, V]): (Map[K, F[V]], Map[K, F[V]]) = {
+                                   (implicit f: F[V] => TraversableGeneric[F, V]): (Map[K, F[V]], Map[K, F[V]]) = {
     (filterMapByValues(map, condition), filterMapByValues(map, (v: V) => !condition(v)))
   }
   object implicits {
-    trait MapFilter[F[_], K, V] {
-      def map: Map[K, F[V]]
-      def filterByValues(condition: V => Boolean)
-                        (implicit iter: F[V] => TraversableGeneric[F, V]): Map[K, F[V]] = {
+    implicit class mapHasTools[F[_], K, V](map: Map[K, F[V]]) extends MapTools[F, K, V] {
+      override def filterByValues(condition: V => Boolean)
+                                 (implicit f: F[V] => TraversableGeneric[F, V]): Map[K, F[V]] = {
         filterMapByValues(map, condition)
       }
-      def partitionByValues(condition: V => Boolean)
-                           (implicit iter: F[V] => TraversableGeneric[F, V]): (Map[K, F[V]], Map[K, F[V]]) = {
+
+      override def partitionByValues(condition: V => Boolean)
+                                    (implicit f: F[V] => TraversableGeneric[F, V]): (Map[K, F[V]], Map[K, F[V]]) = {
         partitionMapByValues(map, condition)
       }
-    }
-    implicit class mapWithTraversableValuesFilter[F[_], K, V](toBeFilteredMap: Map[K, F[V]]) extends MapFilter[F, K, V] {
-      override def map: Map[K, F[V]] = toBeFilteredMap
     }
   }
 }
